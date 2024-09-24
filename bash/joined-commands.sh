84abafed996cd12c9e0e32b37a624615802f7e91
Cy-2004 <cy2004@Chelsea>
2024-09-24 12:02:29 -0400

    in class 9/24

---
+++ bash/joined-commands.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "This is more code" && echo "This is even more code"
+
+#echo "This is a test" || echo "Do something else"
+
+my -foe || echo "There was an error"
+
+mv -baseball || exit 98
+
+echo "This is so much fun"
